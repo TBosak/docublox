fc0363856f2e1b239495e7e52821dac13017e3ab
Tim B <41713011+TBosak@users.noreply.github.com>
2025-04-29 22:10:20 -0500

    Working on image editor

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -33,6 +33,8 @@ import ImageEditor from 'tui-image-editor';
 import '@spectrum-web-components/bundle/elements.js';
 import '@spectrum-web-components/theme/spectrum-two/scale-medium.js';
 import '@spectrum-web-components/theme/spectrum-two/theme-dark.js';
+import { FormsModule } from '@angular/forms';
+
 interface EditorItem extends GridsterItem {
   editor: Editor;
   id: number;
@@ -46,6 +48,7 @@ interface EditorItem extends GridsterItem {
     // TiptapBubbleMenuDirective,
     GridsterComponent,
     GridsterItemComponent,
+    FormsModule,
   ],
   standalone: true,
   templateUrl: './page.component.html',
@@ -60,6 +63,16 @@ export class PageComponent implements AfterViewInit {
   private activeImgEl: HTMLImageElement | null = null;
   private activeTiptap!: Editor;
   private tui!: ImageEditor;
+  brightness = 0;
+  public cropping = false;
+  private originalDataURL = '';
+  penColor = '#ff4040';
+  penSize = 8;
+  textColour = '#ffffff';
+  textSize = 48;
+  private selectedTextId = -1;
+  private penWidth = 8;
+  private drawingPen = false;
   @ViewChild('tuiHost', { read: ElementRef }) host!: ElementRef<HTMLDivElement>;
   @ViewChild('imageInput') imageInput!: ElementRef<HTMLInputElement>;
   @ViewChildren('pageZone', { read: ElementRef })
@@ -382,39 +395,23 @@ export class PageComponent implements AfterViewInit {
   openImageEditor(imgEl: HTMLImageElement, ed: Editor) {
     this.activeImgEl = imgEl;
     this.activeTiptap = ed;
+    this.originalDataURL = imgEl.src;
     this.tui?.destroy?.();
     this.tui = undefined!;
     this.showImageEditor = true;
     this.cdr.detectChanges();
     const hostEl = this.host.nativeElement;
-    hostEl.style.width = '100%'; // use all the free space horizontally
+    hostEl.style.width = '100%';
     hostEl.style.height = '';
     this.tui = new ImageEditor(hostEl, {
       cssMaxWidth: hostEl.offsetWidth,
       cssMaxHeight: Infinity,
       usageStatistics: false,
     });
+    this.hookEditorEvents();
     this.tui.loadImageFromURL(imgEl.src, 'image').then(() => {});
   }
 
-  private cropping = false;
-
-  startCrop() {
-    if (!this.hasEditor) return;
-
-    if (!this.cropping) {
-      this.tui.startDrawingMode('CROPPER');
-      this.cropping = true;
-    } else {
-      const rect = this.tui.getCropzoneRect();
-      if (rect.width && rect.height) {
-        this.tui.crop(rect).then(() => {
-          this.tui.stopDrawingMode();
-          this.cropping = false;
-        });
-      }
-    }
-  }
   rotate() {
     if (this.hasEditor) this.tui.rotate(90);
   }
@@ -466,6 +463,116 @@ export class PageComponent implements AfterViewInit {
       this.tui = undefined!;
     }
     this.showImageEditor = false;
+  }
+
+  startPen() {
+    if (!this.hasEditor) {
+      return;
+    }
+    this.tui.stopDrawingMode();
+    this.tui.startDrawingMode('FREE_DRAWING', {
+      width: this.penWidth,
+      color: this.penColor,
+    });
+    this.drawingPen = true;
+  }
+
+  changePenColour(ev: Event) {
+    this.penColor = (ev.target as HTMLInputElement).value;
+    if (this.drawingPen) {
+      this.tui.setBrush({ width: this.penWidth, color: this.penColor });
+    }
+  }
+
+  onPenSizeChange(size: string | number) {
+    this.penWidth = +size;
+    if (this.drawingPen) {
+      this.tui.setBrush({ width: this.penWidth, color: this.penColor });
+    }
+  }
+
+  addText() {
+    if (!this.hasEditor) {
+      return;
+    }
+    this.tui.stopDrawingMode();
+    const { width, height } = this.tui.getCanvasSize();
+    const position = {
+      x: width / 2,
+      y: height / 2,
+      originX: 'center',
+      originY: 'center',
+    };
+
+    this.tui
+      .addText('Double-click to edit', {
+        position,
+        styles: {
+          fill: this.textColour,
+          fontSize: this.textSize,
+          fontWeight: 'bold',
+        },
+      })
+      .then((obj) => {
+        /* activate so user can drag immediately */
+        (this.tui as any)._graphics
+          .getCanvas()
+          .setActiveObject((this.tui as any)._graphics.getObjectById(obj.id));
+      });
+  }
+
+  reset() {
+    if (!this.hasEditor) return;
+    this.tui.loadImageFromURL(this.originalDataURL, 'reset');
+    this.brightness = 0;
+  }
+
+  startCrop() {
+    if (!this.hasEditor) return;
+
+    if (!this.cropping) {
+      this.tui.startDrawingMode('CROPPER');
+      this.cropping = true;
+    } else {
+      const rect = this.tui.getCropzoneRect();
+      if (rect.width && rect.height) {
+        this.tui.crop(rect).then(() => {
+          this.tui.stopDrawingMode();
+          this.cropping = false;
+        });
+      } else {
+        this.tui.discardSelection();
+        this.tui.stopDrawingMode();
+        this.cropping = false;
+      }
+    }
+  }
+
+  onTextColourChange(hex: string) {
+    this.textColour = hex;
+    if (this.selectedTextId !== -1) {
+      this.tui.changeTextStyle(this.selectedTextId, { fill: this.textColour });
+    }
+  }
+
+  onTextSizeChange(size: string | number) {
+    this.textSize = +size;
+    if (this.selectedTextId !== -1) {
+      this.tui.changeTextStyle(this.selectedTextId, {
+        fontSize: this.textSize,
+      });
+    }
+  }
+
+  private hookEditorEvents() {
+    this.tui.on('objectActivated', ({ type, id, fill, fontSize }) => {
+      if (type === 'i-text') {
+        this.selectedTextId = id;
+        this.textColour = fill as string;
+        this.textSize = fontSize as number;
+      }
+    });
+    this.tui.on('selectionCleared', () => (this.selectedTextId = -1));
   }
 
   set isExporting(value: boolean) {
